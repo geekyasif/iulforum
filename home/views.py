257b52c4d4806b76fdc8a44ae4ae78f0dcaa92be
asif <mdasif08737@gmail.com>
2021-04-10 10:58:14 +0530

    inital commit

---
+++ home/views.py
@@ -0,0 +1,74 @@
+from django.shortcuts import render, redirect
+from django.http import HttpResponse, request
+from .forms import FeedbackForm
+from .models import Feedback
+from blog.models import Post, Category
+from notes.models import Note, Subject
+from papers.models import Paper
+
+# Create your views here.
+def home(request):
+    new_feedback = None
+    if request.method == "POST":
+        feedback_form = FeedbackForm(request.POST)
+        if feedback_form.is_valid():
+            new_feedback = feedback_form.save(commit=False)
+            new_feedback.save()
+            feedback_form = FeedbackForm()
+    else:
+        feedback_form = FeedbackForm()
+    context = {
+        'feedback_form': feedback_form
+    }
+    return render(request, "home/index.html", context)
+
+def about(request):
+    new_feedback = None
+    if request.method == "POST":
+        feedback_form = FeedbackForm(request.POST)
+        if feedback_form.is_valid():
+            new_feedback = feedback_form.save(commit=False)
+            new_feedback.save()
+            feedback_form = FeedbackForm()
+    else:
+        feedback_form = FeedbackForm()
+    context = {
+        'feedback_form': feedback_form
+    }
+    return render(request, 'home/about.html', context)
+
+def search(request):
+    query = request.GET['query']
+    if len(query)>75:
+        allPosts = []
+    else:
+        allPosts = Post.objects.filter(
+            title__icontains=query,
+            description__icontains=query
+        )
+        # allPostsTitle = Post.objects.filter(title__icontains=query)
+        # allPostsContent = Post.objects.filter(description__icontains=query)
+        allnotes = Note.objects.filter(title__icontains=query)     
+        allpapers = Paper.objects.filter(title__icontains=query)     
+        # allPosts = allPostsTitle.union(allPostsContent)
+    
+    # feedback 
+    new_feedback = None
+    if request.method == "POST":
+        feedback_form = FeedbackForm(request.POST)
+        if feedback_form.is_valid():
+            new_feedback = feedback_form.save(commit=False)
+            new_feedback.save()
+            feedback_form = FeedbackForm()
+    else:
+        feedback_form = FeedbackForm()
+    context = {
+        'allPosts' : allPosts,
+        'allnotes': allnotes,
+        'allpapers' : allpapers, 
+        'query' : query,
+        'feedback_form': feedback_form
+    }    
+    return render(request, 'home/search.html', context)
+
+
