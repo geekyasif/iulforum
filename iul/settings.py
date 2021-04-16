5b9cf80cb77a93ac5bc077a551d1cac36f4cb066
Mohammad Asif <38504330+geekyasif@users.noreply.github.com>
2021-04-17 00:03:34 +0530

    Update settings.py
    static files changes

--- iul/settings.py
+++ iul/settings.py
@@ -124,19 +124,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    BASE_DIR / "static",
-    '/var/www/static/',
-]
-
-STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
-STATICFILES_DIRS = (
-   ('iul', os.path.join(BASE_DIR, 'iul', 'static')),
-)
-STATICFILES_FINDERS = (
-  'django.contrib.staticfiles.finders.FileSystemFinder',
-  'django.contrib.staticfiles.finders.AppDirectoriesFinder',
-)
 
 MEDIA_URL	=	'/media/'
 MEDIA_ROOT	=	os.path.join(BASE_DIR,	'media/')
